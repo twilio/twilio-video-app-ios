a9178a3b985cd9c41f8a390bd334d7b660db5e2d
Tim Rozum <trozum@twilio.com>
2019-10-30 14:03:06 -0600

    More builds

--- Scripts/build-app-community.sh
+++
@@ -1,6 +0,0 @@
-xcodebuild \
-  -workspace VideoApp.xcworkspace \
-  -scheme "Video-Community" \
-  -sdk iphonesimulator \
-  -destination 'platform=iOS Simulator,name=iPhone 11,OS=13.0' \
-| xcpretty
