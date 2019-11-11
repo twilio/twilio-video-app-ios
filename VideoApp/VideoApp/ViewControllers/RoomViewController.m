190694979cc4287a12296825fe665f33930b2c29
Tim Rozum <trozum@twilio.com>
2019-11-11 15:23:53 -0700

    Change file header comments to Apache license

--- VideoApp/VideoApp/ViewControllers/RoomViewController.m
+++ VideoApp/VideoApp/ViewControllers/RoomViewController.m
@@ -1,9 +1,17 @@
 //
-//  RoomViewController.m
-//  VideoApp
+//  Copyright (C) 2019 Twilio, Inc.
 //
-//  Created by Ryan Payne on 5/17/17.
-//  Copyright © 2017 Twilio, Inc. All rights reserved.
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 //
 
 #import "RoomViewController.h"
