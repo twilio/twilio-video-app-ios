ec464972b40d01065a6ef2099e959ac75f76c096
Tim Rozum <trozum@twilio.com>
2019-12-30 10:40:47 -0700

    Convert AppDelegate to Swift

---
+++ VideoApp/VideoApp/Launch/AppDelegate.swift
@@ -0,0 +1,71 @@
+//
+//  Copyright (C) 2019 Twilio, Inc.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import TwilioVideo
+import UIKit
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    var launchFlow: LaunchFlow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        AppSettingsStore.shared.start()
+        AuthStore.shared.start()
+        CrashReportStore.shared.start()
+        TwilioVideoSDK.setLogLevel(.info)
+
+        if #available(iOS 13, *) {
+            // Do nothing because SceneDelegate will handle it
+        } else {
+            self.window = UIWindow(frame: UIScreen.main.bounds)
+            self.launchFlow = LaunchFlowFactory().makeLaunchFlow(window: window!)
+            self.launchFlow?.start()
+        }
+
+        return true
+    }
+
+    @available(iOS 13, *)
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        let configuration = UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        configuration.delegateClass = SceneDelegate.self
+        return configuration
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        let navigationController = window?.rootViewController as? UINavigationController
+        
+        guard navigationController?.viewControllers.count == 1 else { return }
+
+        let segueIdentifier = AuthStore.shared.isSignedIn ? "lobbySegue" : "loginSegue"
+        navigationController?.topViewController?.performSegue(withIdentifier: segueIdentifier, sender: self)
+    }
+
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        return AuthStore.shared.openURL(
+            url,
+            sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
+            annotation: options[UIApplication.OpenURLOptionsKey.sourceApplication]
+        )
+    }
+}
