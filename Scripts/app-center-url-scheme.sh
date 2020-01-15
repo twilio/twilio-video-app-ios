f1d0708e6438da73c0b27581592c451c66ed72f4
Tim Rozum <trozum@twilio.com>
2020-01-15 13:00:31 -0700

    Fix CI

--- Scripts/app-center-url-scheme.sh
+++ Scripts/app-center-url-scheme.sh
@@ -6,6 +6,6 @@ echo $INFO_PLIST_FILE
 
 app_center_app_secret=$(jq '.app_center_app_secret' ../Credentials/Credentials.json)
 
-echo $app_center_app_secret
+LC_ALL=C sed -i '' -e "s/APP_CENTER_APP_SECRET_PLACEHOLDER/$app_center_app_secret/" ${CODESIGNING_FOLDER_PATH}/Info.plist
 
-sed -i '' -e "s/APP_CENTER_APP_SECRET_PLACEHOLDER/$app_center_app_secret/" ${CODESIGNING_FOLDER_PATH}/Info.plist
+# Add error handling 
