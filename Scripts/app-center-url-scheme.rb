a0e8b45b2be9742358b1c9e49aafc774e1b72b10
Tim Rozum <trozum@twilio.com>
2020-01-15 15:13:36 -0700

    Use Ruby script for App Center URL scheme

---
+++ Scripts/app-center-url-scheme.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/ruby
+
+require 'json'
+
+puts "Hello Ruby!"
+
+INFO_PLIST_FILE = ENV['CODESIGNING_FOLDER_PATH'] + "/Info.plist"
+# INFO_PLIST_FILE = ENV['SRCROOT'] + "/VideoApp/Info.plist"
+
+
+puts INFO_PLIST_FILE
+
+file = File.read('../Credentials/Credentials.json')
+
+app_center_app_secret = JSON.parse(file)['app_center_app_secret']
+
+text = File.read(INFO_PLIST_FILE)
+replace = text.gsub("APP_CENTER_APP_SECRET_PLACEHOLDER", app_center_app_secret)
+File.open(INFO_PLIST_FILE, "w") {|file| file.puts replace}
+
+
+
+# File.open(INFO_PLIST_FILE, ‘r+’) { |f| newstr = f.read.gsub
+#     ('APP_CENTER_APP_SECRET_PLACEHOLDER', app_center_app_secret); f.rewind; f.puts(newstr) }
