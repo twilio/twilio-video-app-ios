e7a7d951556b521c6120b67737f4fa965e69bae2
Ryan C. Payne <rpayne@twilio.com>
2020-02-21 10:35:31 -0500

    The correct env variable is BINTRAY_API_KEY

--- .circleci/setup_internal_pod_repo.sh
+++ .circleci/setup_internal_pod_repo.sh
@@ -2,5 +2,5 @@
 chmod 600 .circleci/.netrc
 cp .circleci/.netrc ~/.netrc
 sed -i '' "s/BINTRAY_USERNAME/${BINTRAY_USERNAME}/g" ~/.netrc
-sed -i '' "s/BINTRAY_PASSWORD/${BINTRAY_PASSWORD}/g" ~/.netrc
+sed -i '' "s/BINTRAY_API_KEY/${BINTRAY_API_KEY}/g" ~/.netrc
 pod repo add cocoapod-specs-internal git@github.com:twilio/cocoapod-specs-internal.git
