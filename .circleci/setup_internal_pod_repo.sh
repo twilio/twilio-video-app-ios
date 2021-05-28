7aeab3a61c4d07198520d5b1ce2fe3af7dbd4ef2
Tim Rozum <trozum@twilio.com>
2021-05-28 13:56:15 -0600

    Setup artifactory on CI (#161)
    * Replace bintray with artifactory
    
    * Point to internal pod to test CI
    
    * Fix podfile
    
    * Fix Podfile.lock
    
    * Revert pod changes
    
    * Fix pods

--- .circleci/setup_internal_pod_repo.sh
+++ .circleci/setup_internal_pod_repo.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 chmod 600 .circleci/.netrc
 cp .circleci/.netrc ~/.netrc
-sed -i '' "s/BINTRAY_USERNAME/${BINTRAY_USERNAME}/g" ~/.netrc
-sed -i '' "s/BINTRAY_API_KEY/${BINTRAY_API_KEY}/g" ~/.netrc
+sed -i '' "s/ARTIFACTORY_USERNAME/${ARTIFACTORY_USERNAME}/g" ~/.netrc
+sed -i '' "s/ARTIFACTORY_API_KEY/${ARTIFACTORY_API_KEY}/g" ~/.netrc
 pod repo add cocoapod-specs-internal git@github.com:twilio/cocoapod-specs-internal.git
