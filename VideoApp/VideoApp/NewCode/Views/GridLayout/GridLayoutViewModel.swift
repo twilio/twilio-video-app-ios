ea810da3a48f2b7ff2c209d4a7a6fbc585418c34
Tim Rozum <trozum@twilio.com>
2022-03-04 16:27:38 -0700

    GridLayoutViewModel improvements

--- VideoApp/VideoApp/NewCode/Views/GridLayout/GridLayoutViewModel.swift
+++ VideoApp/VideoApp/NewCode/Views/GridLayout/GridLayoutViewModel.swift
@@ -14,7 +14,6 @@
 //  limitations under the License.
 //
 
-import TwilioVideo
 import Combine
 
 /// Subscribes to room and participant state changes to provide participant state for the UI to display in a grid.
@@ -22,10 +21,6 @@ class GridLayoutViewModel: ObservableObject {
     struct Page: Hashable {
         let identifier: Int
         var participants: [ParticipantViewModel]
-
-        func indexOf(identity: String) -> Int? {
-            participants.firstIndex { $0.identity == identity }
-        }
 
         static func == (lhs: Page, rhs: Page) -> Bool {
             lhs.identifier == rhs.identifier
@@ -74,7 +69,8 @@ class GridLayoutViewModel: ObservableObject {
             .sink { [weak self] participant in
                 guard let self = self else { return }
 
-                self.addParticipant(ParticipantViewModel(participant: participant)) }
+                self.addParticipant(ParticipantViewModel(participant: participant))
+            }
             .store(in: &subscriptions)
 
         roomManager.remoteParticipantDisconnectPublisher
@@ -84,30 +80,26 @@ class GridLayoutViewModel: ObservableObject {
         roomManager.remoteParticipantChangePublisher
             .sink { [weak self] participant in
                 guard let self = self else { return }
-
-                self.updateParticipant(ParticipantViewModel(participant: participant)) }
+                
+                self.updateParticipant(ParticipantViewModel(participant: participant))
+            }
             .store(in: &subscriptions)
     }
     
     func addParticipant(_ participant: ParticipantViewModel) {
-        if !pages.isEmpty && pages.last!.participants.count < maxParticipantsPerPage {
-            pages[pages.count - 1].participants.append(participant)
-        } else {
-            let page = Page(identifier: pages.count, participants: [participant])
-            pages.append(page)
-        }
+        pages.appendParticipant(participant, maxParticipantsPerPage: maxParticipantsPerPage)
     }
     
-    private func removeParticipant(with identity: String) {
-        guard let indexPath = pages.IndexPathForParticipant(identity: identity) else {
+    func removeParticipant(with identity: String) {
+        guard let indexPath = pages.indexPathOfParticipant(identity: identity) else {
             return
         }
         
         pages.removeParticipant(at: indexPath)
     }
 
-    private func updateParticipant(_ participant: ParticipantViewModel) {
-        guard let indexPath = pages.IndexPathForParticipant(identity: participant.identity) else {
+    func updateParticipant(_ participant: ParticipantViewModel) {
+        guard let indexPath = pages.indexPathOfParticipant(identity: participant.identity) else {
             return
         }
 
@@ -124,67 +116,91 @@ class GridLayoutViewModel: ObservableObject {
                 let oldestDominantSpeaker = pages[0].participants[1...] // Skip local user at 0
                     .sorted { $0.dominantSpeakerStartTime < $1.dominantSpeakerStartTime }
                     .first!
-
                 let oldestDominantSpeakerIndex = pages[0].participants.firstIndex(of: oldestDominantSpeaker)!
-
                 pages.removeParticipant(at: IndexPath(item: oldestDominantSpeakerIndex, section: 0))
-                pages.insertParticipant(participant, at: IndexPath(item: oldestDominantSpeakerIndex, section: 0))
+                pages.insertParticipant(
+                    participant,
+                    at: IndexPath(item: oldestDominantSpeakerIndex, section: 0),
+                    maxParticipantsPerPage: maxParticipantsPerPage
+                )
                 pages.removeParticipant(at: indexPath)
-                pages.insertParticipant(oldestDominantSpeaker, at: IndexPath(item: 0, section: 1))
+                pages.insertParticipant(
+                    oldestDominantSpeaker,
+                    at: IndexPath(item: 0, section: 1),
+                    maxParticipantsPerPage: maxParticipantsPerPage
+                )
             }
         }
     }
 }
 
-extension Array where Element == GridLayoutViewModel.Page {
-    func IndexPathForParticipant(identity: String) -> IndexPath? {
-        guard
-            let pageIndex = firstIndex(where: { $0.indexOf(identity: identity) != nil }),
-            let participantIndex = self[pageIndex].indexOf(identity: identity)
-        else {
-            return nil
-        }
-        
-        return IndexPath(item: participantIndex, section: pageIndex)
-    }
-    
-    mutating func removeParticipant(at indexPath: IndexPath) {
-        self[indexPath.section].participants.remove(at: indexPath.item)
-        
-        shiftForward(pageIndex: indexPath.section)
-    }
-    
-    mutating func shiftForward(pageIndex: Int) {
-        if pageIndex == count - 1 {
-            // Last page
-            if self[pageIndex].participants.isEmpty {
-                remove(at: pageIndex)
+// TODO: Explain recursive solution.
+private extension Array where Element == GridLayoutViewModel.Page {
+    func indexPathOfParticipant(identity: String) -> IndexPath? {
+        for (section, page) in enumerated() {
+            for (item, participant) in page.participants.enumerated() {
+                if participant.identity == identity {
+                    return IndexPath(item: item, section: section)
+                }
             }
-        } else {
-            self[pageIndex].participants.append(self[pageIndex + 1].participants.removeFirst())
-            shiftForward(pageIndex: pageIndex + 1)
         }
+        
+        return nil
     }
 
-    mutating func insertParticipant(_ participant: ParticipantViewModel, at indexPath: IndexPath) {
-        if indexPath.section > count - 1 {
+    mutating func appendParticipant(_ participant: ParticipantViewModel, maxParticipantsPerPage: Int) {
+        if !isEmpty && last!.participants.count < maxParticipantsPerPage {
+            self[endIndex - 1].participants.append(participant)
+        } else {
+            let newPage = GridLayoutViewModel.Page(identifier: endIndex, participants: [participant])
+            append(newPage)
+        }
+    }
+    
+    mutating func insertParticipant(
+        _ participant: ParticipantViewModel,
+        at indexPath: IndexPath,
+        maxParticipantsPerPage: Int
+    ) {
+        if indexPath.section == endIndex {
             let newPage = GridLayoutViewModel.Page(identifier: indexPath.section, participants: [participant])
             append(newPage)
         } else {
             self[indexPath.section].participants.insert(participant, at: indexPath.item)
-            shiftRight(pageIndex: indexPath.section)
+            shiftRight(pageIndex: indexPath.section, maxParticipantsPerPage: maxParticipantsPerPage)
+        }
+    }
+
+    mutating func removeParticipant(at indexPath: IndexPath) {
+        self[indexPath.section].participants.remove(at: indexPath.item)
+        
+        shiftLeft(pageIndex: indexPath.section)
+    }
+
+    private mutating func shiftLeft(pageIndex: Int) {
+        if pageIndex < endIndex - 1 {
+            self[pageIndex].participants.append(self[pageIndex + 1].participants.removeFirst())
+            shiftLeft(pageIndex: pageIndex + 1)
+        } else if self[pageIndex].participants.isEmpty {
+            // If the last page is empty remove it
+            remove(at: pageIndex)
         }
     }
     
-    mutating func shiftRight(pageIndex: Int) {
-        if self[pageIndex].participants.count > 6 {
-            if pageIndex == count - 1 {
-                let newPage = GridLayoutViewModel.Page(identifier: pageIndex + 1, participants: [self[pageIndex].participants.removeLast()])
-                append(newPage)
-            } else {
-                self[pageIndex + 1].participants.insert(self[pageIndex].participants.last!, at: 0)
-                shiftRight(pageIndex: pageIndex + 1)
-            }
+    private mutating func shiftRight(pageIndex: Int, maxParticipantsPerPage: Int) {
+        guard self[pageIndex].participants.count > maxParticipantsPerPage else {
+            return
+        }
+        
+        if pageIndex == endIndex - 1 {
+            let newPage = GridLayoutViewModel.Page(
+                identifier: pageIndex + 1,
+                participants: [self[pageIndex].participants.removeLast()]
+            )
+            append(newPage)
+        } else {
+            self[pageIndex + 1].participants.insert(self[pageIndex].participants.last!, at: 0)
+            shiftRight(pageIndex: pageIndex + 1, maxParticipantsPerPage: maxParticipantsPerPage)
         }
     }
 }
